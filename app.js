03260325ce14fec9d0d5cb2f69756772304ca2fe
Samuel Tijani <omalitijani@gmail.com>
2020-03-08 11:04:11 +0100

    Add cors

--- app.js
+++ app.js
@@ -13,6 +13,20 @@ const { List, Task } = require('./db/models');
 // Load middleware
 app.use(bodyParser.json());
 
+// CORS HEADERS MIDDLEWARE
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token, x-refresh-token, _id");
+
+    res.header(
+        'Access-Control-Expose-Headers',
+        'x-access-token, x-refresh-token'
+    );
+
+    next();
+});
+
 
 /* ROUTE HANDLERS */
 
